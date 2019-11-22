8b946b892279dd4878d80f118e8bc2b1df51484b
Shen Yang <shen.yang.work.tw@gmail.com>
2019-11-22 16:27:29 +0800

    Initial commit

--- edu_front/js/fullCalendar.js
+++ edu_front/js/fullCalendar.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
     locale: 'zh-tw',
-    plugins: [ 'dayGrid', 'timeGrid', 'list' ],
+    plugins: ['dayGrid', 'timeGrid', 'list'],
     defaultView: 'dayGridMonth',
     header: {
       left: 'prevYear,prev,next,nextYear today',
@@ -26,35 +26,49 @@ document.addEventListener('DOMContentLoaded', function() {
       custom3: {
         text: '增加',
         click: function() {
-          window.open('https://www.google.com/');
+          // window.open('https://www.google.com/');
+          var modal = UIkit.modal($("#add"), {
+            center: true
+          });
+          // var modal = UIkit.modal("#add");
+          if (modal.isActive()) {
+            modal.hide();
+          } else {
+            modal.show();
+          }
         }
       }
     },
     // events: 'https://fullcalendar.io/demo-events.json',
-    events: [
-      {
+    events: [{
         title: 'All Day Event',
         url: 'https://www.google.com/',
-        start: '2019-05-01',
+        start: '2019-11-14',
         backgroundColor: '#ee7502'
       },
       {
         title: 'Long Event',
-        start: '2019-05-07',
-        end: '2019-05-10',
+        start: '2019-11-20',
+        end: '2019-11-22',
         backgroundColor: '#28a745'
       },
       {
         title: 'Event',
-        start: '2019-05-11',
-        end: '2019-05-13',
+        start: '2019-11-23',
+        end: '2019-11-25',
         backgroundColor: '#dc3545'
       },
       {
         title: 'Event',
-        start: '2019-05-21',
+        start: '2019-11-30',
       }
     ],
+    eventClick: function(info) {
+      info.jsEvent.preventDefault(); // don't let the browser navigate
+      if (info.event.url) {
+        window.open(info.event.url);
+      }
+    }
   });
 
   calendar.render();
